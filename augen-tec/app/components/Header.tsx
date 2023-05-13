70adaa9faaaf45ec8b6e1c2f92f7d5da43aaaf71
Leonardo Flores <leonardo.flores.t@outlook.com>
2023-05-12 23:29:39 -0600

    Initialize augen tec project

---
+++ augen-tec/app/components/Header.tsx
@@ -0,0 +1,10 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Grid container>
+      <Grid item></Grid>
+    </Grid>
+  )
+}
