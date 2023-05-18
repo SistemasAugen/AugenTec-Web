7c7a835e875b409457a319ac5bad1a67ab93caf5
Leonardo Flores <leonardo.flores.t@outlook.com>
2023-05-17 23:48:22 -0600

    fix menu navigation issues

--- augen-tec/app/page.tsx
+++ augen-tec/app/page.tsx
@@ -8,7 +8,7 @@ import Menu from './components/asideMenu/menu';
 import YouTube from 'react-youtube';
 import localFont from 'next/font/local';
 import styles from './page.module.css';
-const Raleway = localFont({ src: '../app/public/fonts/Raleway-VariableFont_wght.ttf'});
+const Raleway = localFont({ src: '../app/public/fonts/Raleway-VariableFont_wght.ttf' });
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 import { Grid } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
@@ -24,7 +24,7 @@ const opts = {
 };
 
 // Esto debe coincidir con el id establecido en /public/texts/es.ts
-export const MenuOptions = [ 'AugenTec', 'Sistema', 'Maquinaria', 'Contacto' ];
+export const MenuOptions = ['AugenTec', 'Sistema', 'Maquinaria', 'Contacto'];
 
 export default function Home() {
 
@@ -34,43 +34,41 @@ export default function Home() {
 
   return (
     <AppContextProvider>
-      <div>
-        <main className={Raleway.className}>
-          <div className={styles.stickyMenu}>
-            <Header></Header>
-          </div>
-          <Menu MenuOptions={MenuOptions}/>
-          {esData.main.map((item) => {
-            return (
-              <Content
-                id={item.id}
-                key={item.imageData.alt}
-                imgData={item.imageData}
-                colorTitle={item.colorTitle}
-                title={item.title}
-                text={item.text}></Content>
-            );
-          })}
-          <YouTube className={styles.videoPlayer} videoId={videoId} opts={opts} />
-          {esData.equipos.map((item) => {
-            return (
-              <Equipment
-                id={item.id}
-                key={item.imageData.alt}
-                imgData={item.imageData}
-                colorBorder={item.colorBorder}
-                title={item.title}
-                text={item.text}></Equipment>
-            );
-          })}
-          <Grid container justifyContent={"center"}>
-            <span onClick={handleDownloadBrochureClick} className={styles.downloadButton}>
-              <DownloadForOfflineIcon sx={{ color: "#007960", fontSize: "50px", marginRight: "20px" }} /> Descargar Brochure
-            </span>
-          </Grid>
-          <Contact {...esData.contact}></Contact>
-        </main>
+      <div className={styles.stickyMenu}>
+        <Header></Header>
       </div>
+      <main className={Raleway.className}>
+        <Menu MenuOptions={MenuOptions} />
+        {esData.main.map((item) => {
+          return (
+            <Content
+              id={item.id}
+              key={item.imageData.alt}
+              imgData={item.imageData}
+              colorTitle={item.colorTitle}
+              title={item.title}
+              text={item.text}></Content>
+          );
+        })}
+        <YouTube className={styles.videoPlayer} videoId={videoId} opts={opts} />
+        {esData.equipos.map((item) => {
+          return (
+            <Equipment
+              id={item.id}
+              key={item.imageData.alt}
+              imgData={item.imageData}
+              colorBorder={item.colorBorder}
+              title={item.title}
+              text={item.text}></Equipment>
+          );
+        })}
+        <Grid container justifyContent={"center"}>
+          <span onClick={handleDownloadBrochureClick} className={styles.downloadButton}>
+            <DownloadForOfflineIcon sx={{ color: "#007960", fontSize: "50px", marginRight: "20px" }} /> Descargar Brochure
+          </span>
+        </Grid>
+        <Contact {...esData.contact}></Contact>
+      </main>
     </AppContextProvider>
   );
 }
