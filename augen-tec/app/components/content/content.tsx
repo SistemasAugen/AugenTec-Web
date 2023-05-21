d9f7a5adce36505a6c1b645fe3437a63bc368cb5
Leonardo Flores <lflores@getinsoft.com>
2023-05-20 23:18:38 -0600

    add aos animations

--- augen-tec/app/components/content/content.tsx
+++ augen-tec/app/components/content/content.tsx
@@ -1,10 +1,13 @@
 import { StaticImageData } from "next/image";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "./content.module.css";
 import { Grid } from "@mui/material";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export interface ContentProps {
-  id?: string,
+  id?: string;
   imgData: {
     src: StaticImageData;
     alt: string;
@@ -19,18 +22,58 @@ export default function content({
   title,
   text,
   colorTitle,
-  id
+  id,
 }: ContentProps) {
+  const imageRef = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 900,
+      easing: "ease",
+    });
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add("aos-init", "aos-animate");
+          }, 350);
+        }
+      });
+    });
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
+
+    // Clean up the observer
+    return () => {
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, []);
+
   return (
     <Grid container xs={12} md={6} id={id || title}>
-      <Grid item xs={12} md={5}>
+      <Grid item xs={12} md={5} ref={imageRef} data-aos="fade-up">
         <Image
           src={imgData.src}
           alt={imgData.alt}
-          className={styles.image}>
-        </Image>
+          className={styles.image}></Image>
       </Grid>
-      <Grid xs={12} md={7} item className={styles.textContainer}>
+      <Grid
+        xs={12}
+        md={7}
+        item
+        className={styles.textContainer}
+        ref={textRef}
+        data-aos="fade-right">
         {colorTitle && (
           <span
             className={styles.colorTitle}
--- augen-tec/app/components/equipment/equipment.tsx
+++ augen-tec/app/components/equipment/equipment.tsx
@@ -1,44 +1,115 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./equipment.module.css";
 import Image, { StaticImageData } from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const BorderPosition = {
-    left: 'left',
-    right: 'right'
-}
+  left: "left",
+  right: "right",
+};
 
 export interface EquipmentModel {
-    id?: string,
-    imgData: {
-        src: StaticImageData,
-        alt: string,
-        height?: string,
-        width?: string
-    }
-    colorBorder:  {
-        position: string,
-        color: string,
-    },
-    title: string,
-    text: string,
+  id?: string;
+  imgData: {
+    src: StaticImageData;
+    alt: string;
+    height?: string;
+    width?: string;
+  };
+  colorBorder: {
+    position: string;
+    color: string;
+  };
+  title: string;
+  text: string;
 }
 
-export default function equipment({ imgData, colorBorder, title, text, id }: EquipmentModel) {
+export default function equipment({
+  imgData,
+  colorBorder,
+  title,
+  text,
+  id,
+}: EquipmentModel) {
+  const imageRef = useRef(null);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 900,
+      easing: "ease",
+    });
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            entry.target.classList.add("aos-init", "aos-animate");
+          }, 350);
+        }
+      });
+    });
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
+
+    // Clean up the observer
+    return () => {
+      if (imageRef.current) {
+        observer.unobserve(imageRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <Grid container xs={12} md={6} className={styles.container} id={id || title}>
-        { colorBorder.position == BorderPosition.left && <Grid item xs={1} style={{ backgroundColor: colorBorder.color }}></Grid>}
-        <Grid item xs={11} className={colorBorder.position == BorderPosition.left? styles.containerRight : styles.containerLeft }>
-            <Image
-                src={imgData.src}
-                alt={imgData.alt}
-                className={styles.image}
-                style={{ height: imgData.height || 'auto', width: imgData.width || 'auto'}}>
-            </Image>
-            <span className={styles.title}>{title}</span>
-            <p className={styles.text}>{text}</p>
-        </Grid>
-        { colorBorder.position == BorderPosition.right && <Grid item xs={1} style={{ backgroundColor: colorBorder.color }}></Grid>}
+    <Grid
+      container
+      xs={12}
+      md={6}
+      className={styles.container}
+      id={id || title}>
+      {colorBorder.position == BorderPosition.left && (
+        <Grid item xs={1} style={{ backgroundColor: colorBorder.color }}></Grid>
+      )}
+      <Grid
+        item
+        xs={11}
+        className={
+          colorBorder.position == BorderPosition.left
+            ? styles.containerRight
+            : styles.containerLeft
+        }>
+        <Image
+          ref={imageRef}
+          data-aos="fade-up"
+          src={imgData.src}
+          alt={imgData.alt}
+          className={styles.image}
+          style={{
+            height: imgData.height || "auto",
+            width: imgData.width || "auto",
+          }}></Image>
+        <div
+          ref={textRef}
+          data-aos={`${
+            colorBorder.position == BorderPosition.right
+              ? "fade-left"
+              : "fade-right"
+          }`}>
+          <span className={styles.title}>{title}</span>
+          <p className={styles.text}>{text}</p>
+        </div>
+      </Grid>
+      {colorBorder.position == BorderPosition.right && (
+        <Grid item xs={1} style={{ backgroundColor: colorBorder.color }}></Grid>
+      )}
     </Grid>
-  )
+  );
 }
